af5266b0239842b16c9eca6b03384a8041a12847
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-04 18:05:30 +0300

    fix EmailActions.SendEmail action did not convert SendEmailRequest.Subject to one line string. add EmailTests subject string tests

---
+++ Tests.Gmail/EmailTests.cs
@@ -0,0 +1,51 @@
+﻿using Tests.Gmail.Base;
+using Apps.Gmail.Actions;
+using Apps.Gmail.Models.Requests;
+
+namespace Tests.Gmail;
+
+[TestClass]
+public class EmailTests : TestBase 
+{
+    [TestMethod]
+    public async Task SendEmail_SubjectHasMoreThanOneLine_IsSuccess() 
+    {
+        // Arrange
+        var emailActions = new EmailActions(InvocationContext, FileManager);
+        var request = new SendEmailRequest 
+        {
+            To = "tohafesenkolovep@gmail.com",
+            Subject = "123456\n7890",
+            Message = "test\ntest2"
+        };
+
+        // Act
+        var result = await emailActions.SendEmail(request);
+
+        // Assert
+        Console.WriteLine(result);
+        Assert.IsNotNull(result);
+        Assert.AreEqual("123456 7890", result.Subject);
+    }
+
+    [TestMethod]
+    public async Task SendEmail_SubjectIsEmpty_IsSuccess() 
+    {
+        // Arrange
+        var emailActions = new EmailActions(InvocationContext, FileManager);
+        var request = new SendEmailRequest 
+        {
+            To = "tohafesenkolovep@gmail.com",
+            Subject = "",
+            Message = "test\ntest2"
+        };
+
+        // Act
+        var result = await emailActions.SendEmail(request);
+
+        // Assert
+        Console.WriteLine(result);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(string.Empty, result.Subject);
+    }
+}
